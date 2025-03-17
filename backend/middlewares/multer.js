73ed45aeb09479aad023ae74b77ba4302c3cf880
Papan07 <papanns19@gmail.com>
2025-03-17 17:02:02 +0530

    update

--- backend/middlewares/multer.js
+++ backend/middlewares/multer.js
@@ -1,4 +1,14 @@
 const multer = require("multer");
+const path = require("path");
+
+const userImageStore=multer.diskStorageStore({
+    destination: function (req, file, cb) {
+      cb(null, './userImages/')
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + '-' + file.originalname)
+    }
+  })
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -10,6 +20,27 @@ const storage = multer.diskStorage({
     }
   })
   
-  const uploadUserImage = multer({ storage: storage })
-
-  module.exports = uploadUserImage;
+  const userImags=multer({
+    storage:userImageStore(),
+    limits:{fileSize:5*1024*1024},
+    fileFilter:(req, file, cb) => {
+      const extension=path.extname(file.originalname).toLowerCase();
+      const mimetype = file.mimetype;
+      const allowdExtensions ={
+        jpeg:true,
+        png:true,
+        jpg:true
+      }
+      constAllowedMimeType={
+        'image/jpeg':true,
+        'image/png':true,
+        'image/jpg':true
+      }
+      if(!allowdExtensions[extension] ||!allowedMimeType[mimetype]){
+        cb(new Error('Only .jpeg,.png,.jpg images are allowed!'))
+      }else{
+        cb(null, true)
+      }
+    }
+  })
+  module.exports = userImags;
